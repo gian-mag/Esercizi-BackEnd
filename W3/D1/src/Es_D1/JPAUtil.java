4e2a34a11f229bf2fc256aacdd2616d06ee36aa8
gian-mag <106075481+gian-mag@users.noreply.github.com>
2022-10-12 17:25:39 +0200

    Add files via upload

---
+++ W3/D1/src/Es_D1/JPAUtil.java
@@ -0,0 +1,23 @@
+package Es_D1;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+
+	private static final EntityManagerFactory entityManagerFactory;
+
+	static {
+		try {
+			entityManagerFactory = Persistence.createEntityManagerFactory("D1");
+		} catch (Throwable ex) {
+			System.err.println("Initial EntityManagerFactory creation failed." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static EntityManagerFactory getEntityManagerFactory() {
+		return entityManagerFactory;
+	}
+
+}
